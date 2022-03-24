7a3765307925c723c113d9d7c5323659607048c2
molottva <molottva@list.ru>
2022-03-24 16:24:47 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 public class Main {
+
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long bonus = service.calculate(amount, registered);
+    System.out.println(bonus);
   }
 }
 
